1452ca5b36afb7045d0111f18f2b5500a0d35dfe
DaikiHNisco <daikih@jsw-nis.co.jp>
2017-11-09 17:56:18 +0900

    ファイル追加

--- www/js/controllers/MainMenu.js
+++ www/js/controllers/MainMenu.js
@@ -3,14 +3,16 @@ app.controller('MainMenuCtrl', function($scope,MainMenuService){
     $scope.MainMenuList = null;
     
     $scope.init = function(){
-        $scope.MainMenuList = JSON.parse(JSON.stringify(MainMenuService.getMenus()));       
+        $scope.MainMenuList = JSON.parse(JSON.stringify(MainMenuService.getMenus()));
         for(var i in $scope.MainMenuList){
             console.log("{0}/{1}".format($scope.MainMenuList[i].page , $scope.MainMenuList[i].text));
         }
     };
     
     $scope.menuClick = function(menu){
-        ons.pushPage("{0}{1}.html".format(CMN.Path.Views , menu.page));
+        console.log("{0}{1}.htmlに遷移".format(CMN.Path.Views , menu.page));
+        //$scope.ons.navigator.pushPage("{0}{1}.html".format(CMN.Path.Views , menu.page));
+        myNavigator.pushPage("{0}{1}.html".format(CMN.Path.Views , menu.page));
     };    
     $scope.init();
 });
