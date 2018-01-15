4bab035f4b2d33dc8a41929f854a227551532694
DaikiHNisco <daikih@jsw-nis.co.jp>
2018-01-15 13:49:53 +0900

    1/14作業分

--- www/js/controllers/RouteByTheme.js
+++ www/js/controllers/RouteByTheme.js
@@ -1,12 +1,34 @@
 app.controller('RouteByThemeCtrl', function($scope,GoogleMapService, DbAccessService){
     $scope.theme = null;
-    $scope.slopes = null;
     $scope.map = null;
-    
-    $scope.init = function(){
+    $scope.slopes = null;
+    $scope.markers = null;
+    $scope.selectIdx = 0;
+
+    document.addEventListener("init", function(event) {
         $scope.theme = $scope.myNavi.topPage.data;
         
-        //Googleマップ設定
+        //  Googleマップ設定
+        $scope.map = GoogleMapService.getMap(
+            document.getElementById("map_canvas") , 
+            "",
+            GoogleMapService.getLatLng(
+                "42.3223",
+                "140.958258"
+            )
+        );
+        
+        DbAccessService.GetSlopeByTheme($scope.theme.ThemeId + "Flg", $scope.theme.filvalue).then(function(rows) {
+            $scope.$apply(function(){$scope.slopes = rows;});
+            
+            $scope.createMarkerInfo();
+            
+            $scope.setMarker();
+        });
+        
+    });
+        /*
+    $scope.init = function(){        //Googleマップ設定
         $scope.map = GoogleMapService.getMap(
             document.getElementById("map_canvas") , 
             null,
@@ -36,6 +58,45 @@ app.controller('RouteByThemeCtrl', function($scope,GoogleMapService, DbAccessSer
                 );
             }
         });
+        
+    };*/
+    
+    $scope.createMarkerInfo = function(){
+        $scope.markers = [];
+        for(var i in $scope.slopes){
+            var idx = Number(i)+1;
+            var slope = $scope.slopes[i];
+            
+            var item = GoogleMapService.getMarker(
+                slope.SlopeName,
+                String(idx),
+                GoogleMapService.getLatLng(
+                    slope.Latitude,
+                    slope.Longitude
+                ),
+                $scope.map,
+                ( idx -1 === $scope.selectIdx ) ? CMN.Icon.Flg.Selected : CMN.Icon.Flg.None,
+                changedActeveIndex(idx)
+            );
+            
+            $scope.markers.push(item);
+        }
+    };
+    
+    $scope.setMarker = function(){
+        GoogleMapService.marksToMap($scope.markers, $scope.map);
+    };
+    
+    changedActeveIndex = function(idx){
+        //slopeList.setActiveIndex(idx);
+    };
+    
+    $scope.selectChanged = function(idx){
+        $scope.selectIdx = idx;
+        
+        $scope.createMarkerInfo();
+        
+        $scope.setMarker();
     };
     
     $scope.linkClick = function(slope){
@@ -47,5 +108,24 @@ app.controller('RouteByThemeCtrl', function($scope,GoogleMapService, DbAccessSer
     };
     $scope.getIcons = CMN.Icon.Get;    
     $scope.getPanIcon = CMN.Icon.GetPan;
-    $scope.init();
+    //$scope.init();
 });
+
+/*
+     <section style="width:100%;height:300px;overflow:auto;">
+        <ons-list>
+            <div ng-repeat="(i,slope) in slopes">
+                <ons-list-item modifier="chevron" ng-click="linkClick(slope)">
+                    <!--<img src="img/icon/mark1.png" height="48px" width="48px" style="padding-right:10px;" />-->
+                    <span style="width:210px;">{{i+1}}:{{slope.SlopeName}}</span>  
+                    <span style="width:120px;">                    
+                      <span ng-repeat="pan in getPanIcon(slope.Pan) track by $index">
+                        <img height="20px" width="20px" src="img/icon/{{pan}}.png" />
+                      </span>   
+                    </span>    
+                </ons-list-item>  
+            </div>
+        </ons-list>       
+     </section>
+*/
+
